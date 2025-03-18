420579336f6d1fd7fa42aa386a135f2e4c8340a0
Victor Sanchez Sanchez <victorssanchez@hotmail.es>
2025-03-18 10:18:36 +0100

    implementando logica valor de las cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -46,3 +46,12 @@ const pedirCarta = () =>{
     return carta;
 
 }
+
+const valorCarta = (carta)=>{
+
+    const valor = carta.substring(0, carta.length -1);
+
+    return (isNaN(valor)) ?
+                (valor === 'A' ? 11 : 10)
+                : valor * 1;
+}
